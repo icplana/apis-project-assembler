e81af9908d5fee76c25ac4b5fccdb5775cdaf0e2
Alba Martin <albams51289@gmail.com>
2024-01-12 10:04:37 +0100

    dog branch finished

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const dogsUrl = "https://dog.ceo/api/breeds/image/random/10";
 const catsUrl = "https://api.thecatapi.com/v1/images/search?limit=10";
+
+document.getElementById("dog-title").addEventListener("click", fetchDogsPictures)
+
+function fetchDogsPictures(){
+    fetch(dogsUrl)
+    .then(response =>response.json())
+    .then((data)=>showDogsPictures(data))
+
+}
+function showDogsPictures(data){
+    const dogsDiv = document.getElementById("divDogs");
+    data.message.forEach((data)=>{
+       const img = document.createElement("img");
+       img.src = data;
+       img.classList.add("dog-img");
+       dogsDiv.appendChild(img);
+    })
+}
